9a5fc7780ea5cbb91c8d8aa020c63ffbfd9f4421
Sajjad Ranjbar <sajjadranjbaryazdi@gmail.com>
2022-08-13 22:12:55 +0430

    Update randomkey.py

--- randomkey.py
+++ randomkey.py
@@ -4,3 +4,28 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import numpy as np
 from random import *
+
+
+img2 = mpimg.imread('input4.png')
+imgplot2 = plt.imshow(img2)
+plt.show()
+
+def rnd():
+    r = randint(1, 225)
+    g = randint(1, 225)
+    b = randint(1, 225)
+    resault = (r, g, b)
+    return resault
+
+from random import *
+for i in range(20):
+    for j in range(20):
+        print(rnd())
+        pix3[i, j] = rnd()
+        
+image3.save('test.png')
+
+
+img2 = mpimg.imread('test.png')
+imgplot2 = plt.imshow(img2)
+plt.show()
